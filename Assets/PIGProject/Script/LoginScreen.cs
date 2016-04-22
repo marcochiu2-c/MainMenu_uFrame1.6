b2d4277f2da09926c925c553f8aeabc972d8180c
Shing Yip <shingyip@Shings-iMac.local>
2016-04-22 15:13:43 +0800

    Add loadasync code for loading progress bar

--- Assets/PIGProject/Script/LoginScreen.cs
+++ Assets/PIGProject/Script/LoginScreen.cs
@@ -28,15 +28,15 @@ public class LoginScreen : MonoBehaviour {
 	private bool newFBUserPanelActivated=false;
 	public Button EnterGameButton;
 	public Button EnterGameFBButton;
-//	public bool sentDBSNSCheckRequest = false;
+	//	public bool sentDBSNSCheckRequest = false;
 	bool userCancelledLogin = false;
-
+	
 	string debugText = "";
 	public static bool isSNSInit = false;
 	public static bool isSNSLoggedIn = false;
-
+	
 	public GameObject loadingImageScreen;
-
+	private AsyncOperation async;
 	// Use this for initialization
 	void Awake() {
 		CallLoginScreen ();
@@ -44,30 +44,30 @@ public class LoginScreen : MonoBehaviour {
 	}
 	
 	private void CallLoginScreen(){
-//		ProfileEvents.OnSoomlaProfileInitialized += () => {
-//			debugText += "SoomlaProfile Initialized !"+"\n";
-//			Soomla.SoomlaUtils.LogDebug("LoginScreen", "SoomlaProfile Initialized !");
-//			LoginScreen.isSoomlaInit = true;
-//			SoomlaProfile.Login(
-//				Provider.FACEBOOK,                        // Social Provider
-//				null
-//				);
-//		};
-//		
-//		ProfileEvents.OnLoginFinished += (UserProfile UserProfile, bool autoLogin, string payload) => {
-//			//SoomlaProfile.MultiShare("Ha Ha Ha",
-//			//                         "~/Documents/device-2015-07-17-104246.png");
-//			Soomla.SoomlaUtils.LogDebug("LogInFinish","User: "+UserProfile.ProfileId);
-//			snsURL = UserProfile.ProfileId;
-//			LoginScreen.isSoomlaLoggedIn = true;
-//			//			SoomlaProfile.GetContacts(Provider.FACEBOOK);
-//			//var purchase = StoreInfo.GetPurchasableItemWithProductId("stardust_10")
-//			//      .PurchaseType as PurchaseWithMarket;
-//			//Debug.Log("Price of Star dust 10 pack "+ purchase.MarketItem.MarketPriceAndCurrency);
-//			
-//		};
-//		SoomlaProfile.Initialize ();
-
+		//		ProfileEvents.OnSoomlaProfileInitialized += () => {
+		//			debugText += "SoomlaProfile Initialized !"+"\n";
+		//			Soomla.SoomlaUtils.LogDebug("LoginScreen", "SoomlaProfile Initialized !");
+		//			LoginScreen.isSoomlaInit = true;
+		//			SoomlaProfile.Login(
+		//				Provider.FACEBOOK,                        // Social Provider
+		//				null
+		//				);
+		//		};
+		//		
+		//		ProfileEvents.OnLoginFinished += (UserProfile UserProfile, bool autoLogin, string payload) => {
+		//			//SoomlaProfile.MultiShare("Ha Ha Ha",
+		//			//                         "~/Documents/device-2015-07-17-104246.png");
+		//			Soomla.SoomlaUtils.LogDebug("LogInFinish","User: "+UserProfile.ProfileId);
+		//			snsURL = UserProfile.ProfileId;
+		//			LoginScreen.isSoomlaLoggedIn = true;
+		//			//			SoomlaProfile.GetContacts(Provider.FACEBOOK);
+		//			//var purchase = StoreInfo.GetPurchasableItemWithProductId("stardust_10")
+		//			//      .PurchaseType as PurchaseWithMarket;
+		//			//Debug.Log("Price of Star dust 10 pack "+ purchase.MarketItem.MarketPriceAndCurrency);
+		//			
+		//		};
+		//		SoomlaProfile.Initialize ();
+		
 		if (!FB.IsInitialized) {
 			// Initialize the Facebook SDK
 			FB.Init(InitCallback, OnHideUnity);
@@ -75,25 +75,25 @@ public class LoginScreen : MonoBehaviour {
 			// Already initialized, signal an app activation App Event
 			FB.ActivateApp();
 		}
-
+		
 		game = Game.Instance;
 		wsc = WsClient.Instance;
 		deviceId = SystemInfo.deviceUniqueIdentifier;
-
+		
 		// TODO send device ID to server for checking user existance
 		JSONNode json = new JSONClass ();
 		wsc.Send ("getUserInformationByDeviceId", "GET", new JSONData (deviceId));
-
-
-//		Debug.Log ("NewFBUserPanel Active: " + newFBUserPanel.activeSelf);
+		
+		
+		//		Debug.Log ("NewFBUserPanel Active: " + newFBUserPanel.activeSelf);
 	}
 	
 	// Update is called once per frame
 	void Update () {
-//		Debug.Log (game.login.snsType);
+		//		Debug.Log (game.login.snsType);
 		if (user!=null){
-
-
+			
+			
 			if (user.ToString ()=="{\"success\":false}") {
 				Debug.Log ("user not existed");
 				
@@ -101,56 +101,56 @@ public class LoginScreen : MonoBehaviour {
 			}else if (LoginScreen.user != null) {
 				var j = LoginScreen.user["attributes"];
 				game.login = new Login ((JSONClass)LoginScreen.user);
-//				Debug.Log (user);
+				//				Debug.Log (user);
 				LoginScreen.user = null;
 			}
-
+			
 			user =null;
 		}
-
+		
 		if (game.login.snsType==1){  // if account already login with SNS
-//			Debug.Log ("account already login with SNS");
+			//			Debug.Log ("account already login with SNS");
 			LoginSNS();
 		}	
-//		}else if (game.login.snsType==0 && game.login.id==0){// not yet registered with SNS
-//			//TODO show Facebook register reminder first
-//
-//			GotoMainUI();
-//		}
+		//		}else if (game.login.snsType==0 && game.login.id==0){// not yet registered with SNS
+		//			//TODO show Facebook register reminder first
+		//
+		//			GotoMainUI("MainMenuScene");
+		//		}
 		if (userWithSns != null)
 			Debug.Log (snsURL);
 		if (newFBUserPanelActivated) {  // For device not link with SNS account and user choose to link with SNS
-
+			
 			if(game.login.id!=0){
 				if(userWithSns!=null && snsURL!=""){ 
 					Debug.Log (userWithSns.ToString ());
 					if (userWithSns.ToString() == "{\"success\":false}") {
 						Debug.Log ("No SNS account found in DB");
 						SetupSocialNetworkAccount();
-						GotoMainUI ();
+						GotoMainUI ("MainMenuScene");
 					} else {
 						Debug.Log ("SNS account found.");
-						GotoMainUI ();
+						GotoMainUI ("MainMenuScene");
 					}
 					MainScene.UserInfo = userWithSns;
 					userWithSns = null;
 				}
 			}
-
+			
 			
 		}
-
-
+		
+		
 	}
-
+	
 	public void EnterGameNoSNS(){
 		if (game.login.id != 0) {
-			GotoMainUI ();
+			GotoMainUI ("MainMenuScene");
 		} else {
 			newUserPanel.SetActive (true);
 		}
 	}
-
+	
 	public void SetupStandaloneAccount(){
 		if (NewUserStandAloneName.text.Trim () == "") {
 			return;
@@ -166,7 +166,11 @@ public class LoginScreen : MonoBehaviour {
 	
 	public void SetupSocialNetworkAccount(){
 		if (NewUserSNSAccountName.text.Trim () == "") {
-			return;
+			Debug.Log ("User id: "+game.login.id);
+			if (game.login.id==0){
+				
+				return;
+			}
 		}
 		if (game.login.id==0) { // Link SNS with new user
 			JSONClass json = new JSONClass ();
@@ -181,12 +185,12 @@ public class LoginScreen : MonoBehaviour {
 		} else {  // Link SNS with old user
 			JSONClass data = new JSONClass ();
 			JSONClass json = new JSONClass ();
-
+			
 			data.Add ("id",new JSONData(game.login.id));   
 			data.Add ("snsType",new JSONData(1)); // For facebook only
 			data.Add ("snsURL", new JSONData(snsURL));
 			data.Add ("deviceId", new JSONData(SystemInfo.deviceUniqueIdentifier));
-
+			
 			json.Add ("action", new JSONData("SET"));
 			json.Add ("table", new JSONData("sns"));
 			json.Add ("data", data);
@@ -201,60 +205,68 @@ public class LoginScreen : MonoBehaviour {
 		json.Add ("table", new JSONData("users"));
 		json.Add ("data", jData);
 		wsc.conn.Send (json.ToString ());
-
-		GotoMainUI ();
+		
+		GotoMainUI ("MainMenuScene");
 	}
 	
-	private void GotoMainUI(){
-//		ClickToLoadAsync cla = new ClickToLoadAsync();
-//		cla.loadingBar = slider;
-//		cla.loadingImage = loadImage;
-//		cla.ClickAsync(5);
-//		loadingImageScreen.SetActive (true);
+	private void GotoMainUI(string level){
+		//		ClickToLoadAsync cla = new ClickToLoadAsync();
+		//		cla.loadingBar = slider;
+		//		cla.loadingImage = loadImage;
+		//		cla.ClickAsync(5);
+		loadingImageScreen.SetActive (true);
 		newUserPanel.SetActive (false);
 		newFBUserPanel.SetActive (false);
 		EnterGameButton.gameObject.SetActive (false);
 		EnterGameFBButton.gameObject.SetActive (false);
 		Debug.Log ("User ID: "+game.login.id);
-		Application.LoadLevel ("MainMenuScene");
+		StartCoroutine (LoadLevelWithBar ("MainMenuScene"));
 	}
-
+	
+	IEnumerator LoadLevelWithBar (string level){
+		async = Application.LoadLevelAsync("MainMenuScene");
+		while(!async.isDone){
+			slider.value=async.progress;
+			yield return null;
+		}	
+	}
+	
 	private bool LoginSNS(){
 		Debug.Log ("Logging in with SNS");
 		if (LoginScreen.isSNSInit){
 			var perms = new List<string>(){"public_profile", "email", "user_friends","user_photos"};
 			FB.LogInWithReadPermissions(perms, AuthCallback);
-
-//			SoomlaProfile.Login(
-//				Provider.FACEBOOK,                        // Social Provider
-//				null
-//				);
+			
+			//			SoomlaProfile.Login(
+			//				Provider.FACEBOOK,                        // Social Provider
+			//				null
+			//				);
 		}
 		return LoginScreen.isSNSInit;
 	}
-
+	
 	public void ActivateNewFBUserPanel(){
 		newFBUserPanel.SetActive (true);
 		newFBUserPanelActivated = true;
 		userCancelledLogin = false;
 		LoginSNS ();
 	}
-
+	
 	public void CheckUserIn(){
 		wsc.Send ("login", "SET", new JSONData (game.login.id));
 	}
-
+	
 	public void CheckUserSNSInDB(){
-//		Debug.Log ("User not linked with SNS");
-//		JSONNode json = new JSONClass ();
-//		json ["data"] = snsURL;
-//		json ["action"] = "GET";
-//		json ["table"] = "getUserInformationBySnsUrl";
-//		wsc.conn.Send (json.ToString ());
+		//		Debug.Log ("User not linked with SNS");
+		//		JSONNode json = new JSONClass ();
+		//		json ["data"] = snsURL;
+		//		json ["action"] = "GET";
+		//		json ["table"] = "getUserInformationBySnsUrl";
+		//		wsc.conn.Send (json.ToString ());
 		wsc.Send ("getUserInformationBySnsUrl", "GET", snsURL);
-//		sentDBSNSCheckRequest = true;
+		//		sentDBSNSCheckRequest = true;
 	}
-
+	
 	private void InitCallback ()
 	{
 		if (FB.IsInitialized) {
@@ -278,7 +290,7 @@ public class LoginScreen : MonoBehaviour {
 			Time.timeScale = 1;
 		}
 	}
-
+	
 	private void AuthCallback (ILoginResult result) {
 		if (FB.IsLoggedIn) {
 			// AccessToken class will have session details
@@ -290,15 +302,13 @@ public class LoginScreen : MonoBehaviour {
 			foreach (string perm in aToken.Permissions) {
 				Debug.Log(perm);
 			}
-
-//			StartCoroutine(FbGetPicture(snsURL));
-
-			CheckUserIn();
+			
+			
 			if (game.login.snsType==0){
 				SetupSocialNetworkAccount();
 			}
-
-			GotoMainUI();
+			CheckUserIn();
+			GotoMainUI("MainMenuScene");
 			if (game.login.id!=0){
 				isSNSLoggedIn=true;
 				CheckUserSNSInDB();
@@ -309,6 +319,6 @@ public class LoginScreen : MonoBehaviour {
 			newFBUserPanel.SetActive(false);
 		}
 	}
-
-
+	
+	
 }
